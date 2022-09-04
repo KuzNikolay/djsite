7a706a6a6f94a397148de699f7dab1d7a196e413
Nikolai <nikolai1kuz@gmail.com>
2022-09-04 10:50:14 +0300

    -3 slug Django lesson by S.Balakiriev
    https://www.youtube.com/watch?v=FyTL1bnUx5I&list=PLA0M1Bcd0w8xO_39zZll2u1lz_Q-Mwn1F
    4.09.2022

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,6 +5,5 @@ from women.views import *
 
 urlpatterns = [
     path('', index), # http://127.0.0.1:8000/
-    path('cats/<int:catid>/', categories),# http://127.0.0.1:8000/cats/<catid>
-
+    path('cats/<slug:cat>/', categories),# http://127.0.0.1:8000/cats/<cat>
 ]
