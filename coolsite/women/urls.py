a37bb84434a920ff495b3170c9a1d495bf528887
Nikolai <nikolai1kuz@gmail.com>
2022-09-09 10:38:42 +0300

    6 Django lesson by S.Balakiriev
    templates
    
    09.09.2022

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,6 +5,7 @@ from women.views import *
 
 urlpatterns = [
     path('', index, name='home'), # http://127.0.0.1:8000/
-    path('cats/<int:catid>/', categories),# http://127.0.0.1:8000/cats/catid
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive), # http://127.0.0.1:8000/archive/2022
+    # path('cats/<int:catid>/', categories),# http://127.0.0.1:8000/cats/catid
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive), # http://127.0.0.1:8000/archive/2022
+    path('about/', about, name='about'), # http://127.0.0.1:8000/about/
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,20 +1,29 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+from .models import *
 
-# Create your views here.
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
 def index(request):
-    return HttpResponse("Страница приложения Women")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О Сайте'})
 
 def categories(request, catid):
     if request.GET:
         print(request.GET)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
+
 def archive(request, year):
     if int(year) > 2022:
         return redirect('home', permanent=True)
     return HttpResponse(f"<h1>Архв по годам</h1><p>{year}</p>")
+
 
 def pageNotFaund(request, exception):
     return HttpResponseNotFound(f'<h1>Страница не найдена</h1><p>СОВСЕМ НЕ НАЙДЕНА</p>')
