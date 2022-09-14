a780eb66e2ad698fece45417803c3c006157ea92
Nikolai <nikolai1kuz@gmail.com>
2022-09-14 20:38:01 +0300

    12 Django lesson by S.Balakiriev slug, get_object_or_404() prepopulated_fields
    13.09.2022

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
@@ -33,14 +33,23 @@ def contact(request):
 def login(request):
     return HttpResponse("Авторизация")
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+    return render(request, 'women/post.html', context=context)
 
 def pageNotFaund(request, exception):
     return HttpResponseNotFound(f'<h1>Страница не найдена</h1><p>СОВСЕМ НЕ НАЙДЕНА</p>')
 
-def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+def show_category(request, cat_slug):
+    cat = Category.objects.filter(slug=cat_slug)
+    posts = Women.objects.filter(cat_id=cat[0].id)
+
 
     if len(posts) == 0:
         raise Http404()
@@ -48,7 +57,7 @@ def show_category(request, cat_id):
     context_index = {
         'posts': posts,
         'menu': menu,
-        'title': f'Категория: {cat_id}',
-        'cat_selected': cat_id,
+        'title': f'Категория: {cat_slug}',
+        'cat_selected': cat_slug,
     }
     return render(request, 'women/index.html', context=context_index)
