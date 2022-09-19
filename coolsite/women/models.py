246b750adf96979045b20f3bb73c67906e411dc7
Nikolai <nikolai1kuz@gmail.com>
2022-09-19 15:53:03 +0300

    17 Django lesson by S.Balakiriev LoginRequiredMixin и декоратор login_required.
    19.09.2022

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -3,14 +3,14 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255, verbose_name = "Заголовок")
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
-    content = models.TextField(blank=True, verbose_name = "Текст статьи")
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name = "Фото")
-    time_create = models.DateTimeField(auto_now_add=True, verbose_name = "Время создания")
-    time_update = models.DateTimeField(auto_now =True, verbose_name = "Время изменения")
-    is_published = models.BooleanField(default=True, verbose_name = "Опубликовано")
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name = "Категория")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категория")
 
     def __str__(self):
         return self.title
@@ -18,14 +18,14 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
 
-
     class Meta:
-        verbose_name = "Известные женщины" # имя в Админке
-        verbose_name_plural = "Известные женщины" # имя в Админке `s (мн. число)
+        verbose_name = "Известные женщины"  # имя в Админке
+        verbose_name_plural = "Известные женщины"  # имя в Админке `s (мн. число)
         ordering = ['-time_create', 'title']
 
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True, verbose_name = 'Категория')
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,108 +1,73 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
 from .models import *
+from .utils import *
 
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
-        ]
 
-class WomenHome(ListView):
+class WomenHome(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
-    # extra_context = {'title': 'Главная страница'}
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Главная страница'
-        context['cat_selected'] = 0
-        return context
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Women.objects.filter(is_published=True)
 
 
-# def index(request):
-#     posts = Women.objects.all()
-#
-#     context_index = {
-#         'posts': posts,
-#         'menu': menu,
-#         'title': 'Главная страница',
-#         'cat_selected': 0,
-#         }
-#     return render(request, 'women/index.html', context = context_index)
-
+# @login_required # запрет для неавторизованных пользователей для функций
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О Сайте'})
 
-class AddPage(CreateView):
+
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
-    success_url = reverse_lazy('home') # Перенаправляет на Home
+    success_url = reverse_lazy('home')  # Перенаправляет на Home
+    # login_url = '/admin/'
+    login_url = reverse_lazy('home') # запрет для неавторизованных пользователей LoginRequiredMixin
+    raise_exception = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Добавление статьи'
-        return context
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
 
-
-# def addpage(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST,request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#
-#     else:
-#         form = AddPostForm()
-#     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 def contact(request):
     return HttpResponse("Обратная связь")
+
 
 def login(request):
     return HttpResponse("Авторизация")
 
 
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Women
     template_name = 'women/post.html'
     slug_url_kwarg = 'post_slug'
-    # pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = context['post']
-        # context['cat_selected'] = None
-        return context
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
-
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Women, slug=post_slug)
-#     context = {
-#         'post': post,
-#         'menu': menu,
-#         'title': post.title,
-#         'cat_selected': post.cat_id,
-#     }
-#     return render(request, 'women/post.html', context=context)
 
 def pageNotFaund(request, exception):
     return HttpResponseNotFound(f'<h1>Страница не найдена</h1><p>СОВСЕМ НЕ НАЙДЕНА</p>')
 
 
-class WomenCategory(ListView):
+class WomenCategory(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
@@ -110,27 +75,9 @@ class WomenCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        context['cat_selected'] = context['posts'][0].cat_id
-        return context
+        c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
+                                      cat_selected=context['posts'][0].cat_id)
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
-
-
-# def show_category(request, cat_slug):
-#     cat = Category.objects.filter(slug=cat_slug)
-#     posts = Women.objects.filter(cat_id=cat[0].id)
-#
-#
-#     if len(posts) == 0:
-#         raise Http404()
-#
-#     context_index = {
-#         'posts': posts,
-#         'menu': menu,
-#         'title': f'Категория: {cat_slug}',
-#         'cat_selected': cat_slug,
-#     }
-#     return render(request, 'women/index.html', context=context_index)
