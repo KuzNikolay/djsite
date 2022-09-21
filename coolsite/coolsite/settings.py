e759b957d86dfd44ff42e7f4ddcc37ed047f4794
Nikolai <nikolai1kuz@gmail.com>
2022-09-21 12:38:10 +0300

    21 Django lesson by S.Balakiriev Оптимизация Django Debug Toolbar
    21.09.2022

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-from django.conf.global_settings import STATIC_ROOT, LOGIN_REDIRECT_URL
+from django.conf.global_settings import STATIC_ROOT, LOGIN_REDIRECT_URL, INTERNAL_IPS
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'women.apps.WomenConfig',
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'coolsite.urls'
@@ -131,3 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # LOGIN_REDIRECT_URL = '/'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
